99b83aba583d8039976cc708a93f86dd8144568c
AndrzejSwietek2001 <mr.andi321@gmail.com>
2021-05-03 19:38:30 +0200

    editor style

--- src/main/kotlin/sparkserver/app.kt
+++ src/main/kotlin/sparkserver/app.kt
@@ -12,8 +12,8 @@ fun main() {
     port(getHerokuPort())
 
     get("/") { req,res -> "index.html" } // get pliku index.html
-    get("/game") { req,res -> "game.html" } // get pliku game.html
-    get("/editor") { req,res -> "editor.html" } // get pliku editor.html
+    get("/game") { req,res -> serveGame(req,res) } // get pliku game.html
+    get("/editor") { req,res -> serveEditor(req,res) } // get pliku editor.html
     post("/add") { req,res -> add(req,res) } // dodanie danych levelu
     post("/load") { req,res -> load(req,res) } // pobranie danych levelu
 
@@ -24,6 +24,16 @@ fun getHerokuPort(): Int {
     return if (processBuilder.environment()["PORT"] != null) {
         processBuilder.environment()["PORT"]!!.toInt()
     } else 5000
+}
+
+fun serveGame(req: Request,res: Response) {
+    res.type("text/html")
+    res.redirect("game.html")
+}
+
+fun serveEditor(req: Request,res: Response) {
+    res.type("text/html")
+    res.redirect("editor.html")
 }
 
 fun add(req: Request, res: Response){
